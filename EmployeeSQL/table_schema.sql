816c2df009dba81aaaadcf1e45924e3c23693205
Daniel <danielmolina81@hotmail.com>
2024-04-25 23:24:58 -0400

    schema

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -30,21 +30,24 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE salaries(
-	emp_no INT UNIQUE,
+	emp_no INT,
 	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_emp(
 	emp_no INT,
 	dept_no VARCHAR(4),
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(4),
-	emp_no INT UNIQUE,
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT,
+	PRIMARY KEY (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
