002f6cbae0f613641ded85382e3d06ecb5c77d3c
Daniel <danielmolina81@hotmail.com>
2024-04-25 22:48:39 -0400

    schema

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,41 +1,50 @@
 DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS titles;
 
 CREATE TABLE titles(
-	title_id VARCHAR(5) PRIMARY KEY,
-	title VARCHAR(30) NOT NULL UNIQUE
+	title_id VARCHAR(5),
+	title VARCHAR(30) NOT NULL UNIQUE,
+	PRIMARY KEY (title_id)
 );
-	
+
+CREATE TABLE departments(
+	dept_no VARCHAR(4),
+	dept_name VARCHAR(30) NOT NULL UNIQUE,
+	PRIMARY KEY (dept_no)
+);
+
 CREATE TABLE employees(
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(5) REFERENCES titles(title_id),
+	emp_no INT,
+	emp_title_id VARCHAR(5) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30)NOT NULL,
 	sex CHAR(1) NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE salaries(
-	emp_no INT UNIQUE REFERENCES employees(emp_no),
-	salary INT NOT NULL
-);
-
-CREATE TABLE department(
-	dept_no VARCHAR(4) PRIMARY KEY,
-	dept_name VARCHAR(30) NOT NULL UNIQUE
+	emp_no INT UNIQUE,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_emp(
-	emp_no INT REFERENCES employees(emp_no),
-	dept_no VARCHAR(4) REFERENCES department(dept_no)
+	emp_no INT,
+	dept_no VARCHAR(4),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(4) REFERENCES department(dept_no),
-	emp_no INT UNIQUE REFERENCES employees(emp_no)
+	dept_no VARCHAR(4),
+	emp_no INT UNIQUE,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
